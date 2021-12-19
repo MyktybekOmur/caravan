281bc7a73f837dd2594f9fdbf09641f4ba46d9bb
MyktybekOmur <mykty.omurbekov@gmail.com>
2021-12-19 11:40:16 +0300

    test refresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
   imports: [BrowserModule, AppRoutingModule, FormsModule, NoopAnimationsModule],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
